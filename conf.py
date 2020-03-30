e3f39be1d9774e45b0abe9fb5916ab2f6cf33828
Gideon <gideon.zweijtzer@gmail.com>
2020-03-30 21:56:55 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -54,8 +54,11 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+def setup(app):
+    app.add_stylesheet('_static/theme_overrides.css')
+    
+#html_context = {
+#    'css_files': [
+#        '_static/theme_overrides.css',  # override wide tables in RTD theme
+#        ],
+#     }
