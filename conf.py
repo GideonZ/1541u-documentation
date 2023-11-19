7a6797ad889f21493f84331d11a8b5eba7581fe1
GideonZ <gideon.zweijtzer@gmail.com>
2023-11-19 13:09:32 +0100

    Test formatting options.

--- conf.py
+++ conf.py
@@ -47,7 +47,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#import guzzle_sphinx_theme
+
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_local']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,7 +60,7 @@ html_theme = 'alabaster'
 #html_static_path = ['_static']
 
 def setup(app):
-    app.add_stylesheet('_local/theme_overrides.css')
+    app.add_css_file('theme_overrides.css')
     
 #html_context = {
 #    'css_files': [
